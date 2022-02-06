db4c2f88baea2b7456b850182a01c404ae7b3d0f
aibinary <harikanth.ai@gmail.com>
2022-02-06 09:44:21 +0530

    Added Immutable contract

---
+++ Immutable.sol
@@ -0,0 +1,12 @@
+//SPDX_License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Immutable{
+    address public immutable MY_ADDRESS;
+    uint public immutable MY_UINT;
+
+    constructor(uint _myUint){
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
